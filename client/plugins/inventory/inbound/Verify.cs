9b4225dbba9da641e1d148a873f45f0a4464a23b
nfoxlz <nfox_lz@hotmail.com>
2024-12-27 09:15:32 +0800

    修正一些应用级Bug。

--- client/plugins/inventory/inbound/Verify.cs
+++ client/plugins/inventory/inbound/Verify.cs
@@ -1,8 +1,4 @@
-var dataView = new DataView(data.Tables["master"])
-{
-	RowStateFilter = DataViewRowState.Added | DataViewRowState.ModifiedCurrent
-};
-foreach (DataRowView row in dataView)
+foreach (DataRow row in data.Tables["master"].Rows)
 	if ((decimal)row["Real_Inbound_Quantity"] + (decimal)row["Discount_Inbound_Quantity"] == 0)
 		return "Message.InboundQuantityZero";
 return string.Empty;
--- client/plugins/inventory/outbound/Verify.cs
+++ client/plugins/inventory/outbound/Verify.cs
@@ -1,8 +1,4 @@
-var dataView = new DataView(data.Tables["master"])
-{
-	RowStateFilter = DataViewRowState.Added | DataViewRowState.ModifiedCurrent
-};
-foreach (DataRowView row in dataView)
+foreach (DataRow row in data.Tables["master"].Rows)
 	if ((decimal)row["Real_Outbound_Quantity"] + (decimal)row["Discount_Outbound_Quantity"] == 0M)
 		return "Message.OutboundQuantityZero";
 return string.Empty;
